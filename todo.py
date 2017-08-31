ac377e08fe003394dc0402a65fe5e2afc6080e97
Prasanna Inamdar <prasanna.inamdar77@gmail.com>
2017-08-31 01:11:48 -0500

    Database URL added

--- todo.py
+++ todo.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, render_template, Response
 from flask_restful import Resource, Api, reqparse
 
@@ -9,7 +10,7 @@ from flask_jwt import JWT
 from security import authenticate, identity
 
 app=Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Abcd@123456@localhost/learningflask'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','postgresql://postgres:Abcd@123456@localhost/learningflask')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api=Api(app)
 app.secrete_key = 'Prasanna'
